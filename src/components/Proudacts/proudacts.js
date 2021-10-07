66d36d6c85e4f1c7c2acd6ba882cf98dab64a864
Talya-Natania <elna13772@gmail.com>
2021-10-08 00:09:44 +0300

    first commit

---
+++ src/components/Proudacts/proudacts.js
@@ -0,0 +1,19 @@
+import Proudact from "../Proudact/proudact";
+import "./Proudacts.css";
+function Proudacts() {
+  return (
+    <section className="products">
+      <Proudact />
+      <Proudact />
+      <Proudact />
+      <Proudact />
+      <Proudact />
+      <Proudact />
+      <Proudact />
+      <Proudact />
+      <Proudact />
+      <Proudact />
+    </section>
+  );
+}
+export default Proudacts;
